b83e534b4e3414b47080748245877c292f130752
Khosbayar Ariunbat <74040410+Two-2-Two@users.noreply.github.com>
2021-10-02 18:02:43 +0800

    Complete labortary 2

--- src/laboratories/Lab_2.java
+++ src/laboratories/Lab_2.java
@@ -1,5 +1,246 @@
+/** Chain extended to include methods
+  * clear and add (at end).  */
+
 package laboratories;
 
-public class Lab_2 {
+import java.util.*; 
 
+import dataStructures.*;
+
+public class Lab_2 extends Chain{
+	
+	public Object[] toArray(Chain chain)  {
+		Object[] javaObjectArray = new Object[size]; 
+		for(int i = 0; i < size; i++) {
+			javaObjectArray[i] = chain.get(i);
+		}
+		return javaObjectArray; 
+	}
+	
+	public Chain removeDuplicateElements(Chain chain) {
+		LinkedHashSet<Integer> set = new LinkedHashSet<Integer>();
+		Chain removeElement = new Chain(); 
+		try {
+			for (int i = 0; i < chain.size(); i++) {
+	            set.add(Integer.parseInt(chain.get(i).toString()));
+			}
+	        Iterator value = set.iterator();
+	        int count = 0; 
+			while(value.hasNext()) {
+				removeElement.add(count, value.next());
+				count++; 
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return removeElement;  
+	}
+	
+	
+	public void addRange(Chain chain) {
+		Scanner scanner = new Scanner(System.in);
+		int totalNum = scanner.nextInt();
+		chain.add(chain.size(), totalNum);
+		System.out.println(chain.toString());
+	}
+	
+	public Chain union(Chain chain) {
+		Chain unionChain = new Chain(); 
+		Random randomNum = new Random(); 
+		for(int i = 0; i < chain.size(); i++) {
+			unionChain.add(i, chain.get(i));
+		}
+		Chain anotherChain = new Chain();
+		int totalNum = 10; 
+ 		for(int i = 0; i < totalNum; i++) {
+			int num = randomNum.nextInt(10); 
+			anotherChain.add(i, num);
+			Boolean flag = true; 
+			for(int j = 0; j < unionChain.size(); j++) {
+				if(Integer.parseInt(unionChain.get(j).toString()) == Integer.parseInt(anotherChain.get(i).toString())) {
+					flag = false; 
+				}
+			}
+			if(flag) {
+				unionChain.add(size, anotherChain.get(i));
+			}
+		}
+ 		System.out.println("Үндсэн жагсаалт: " + chain.toString());
+ 		System.out.println("Өгөгдсөн жагсаалт: " + anotherChain.toString());
+ 		return unionChain; 
+	}
+	
+	public Chain intersection(Chain chain) {
+		Chain unionChain = new Chain(); 
+		Random randomNum = new Random(); 
+		Chain anotherChain = new Chain();
+		int totalNum = 10; 
+		int count = 0; 
+ 		for(int i = 0; i < totalNum; i++) {
+			int num = randomNum.nextInt(10); 
+			anotherChain.add(i, num);
+			Boolean flag = false; 
+			for(int j = 0; j < chain.size(); j++) {
+				if(Integer.parseInt(chain.get(j).toString()) == Integer.parseInt(anotherChain.get(i).toString())) {
+					flag = true; 
+					break; 
+				}
+			}
+			if(flag) {
+				unionChain.add(count, anotherChain.get(i));
+				count++; 
+			}
+		}
+ 		
+ 		System.out.println("Үндсэн жагсаалт: " + chain.toString());
+ 		System.out.println("Өгөгдсөн жагсаалт: " + anotherChain.toString());
+ 		return unionChain; 
+	}
+	
+	public int readNumber(Scanner reader) {
+	    while (true) {
+	        try {
+	            int num = Integer.parseInt(reader.nextLine());
+	            return num;
+	        } catch (Exception e) {
+				System.out.println("Та бүхэл тоо оруулна уу.");
+	        }
+	    }
+   }
+	
+	public static void main(String[] args) {
+		 Boolean flag = false;
+		 Boolean bye = false;
+		 Boolean isShow = true;
+		 
+		 Lab_2 arrayLinearList = new Lab_2();
+		 Scanner scanner = new Scanner(System.in);
+		 System.out.println("\n-----------------------------");
+		 System.out.println(" Нэмж тодорхойлсон үйлдэлүүд");
+		 System.out.println("-----------------------------");
+		 
+
+		 System.out.println("1. toArray-Массивруу хөрвүүлэх.");
+		 System.out.println("2. addRange-Шинэ элемент нэмэх.");
+		 System.out.println("3. union-Нэгдэл жагсаалтыг харах.");
+		 System.out.println("4. intersection-Огтлолцол жагсаалтыг харах.");
+		 System.out.println("0. Гарах");
+		 System.out.println("-----------------------------\n");
+		 
+		 System.out.println("Хэдэн тоо оруулах вэ ?");
+		 int srcNum = arrayLinearList.readNumber(scanner);
+		 if (srcNum != 0) { 
+			 System.out.println("Chain -нд оруулах "+ srcNum + " тоогоо оруулна уу.");
+		 }
+		 
+		 for (int i = 0; i < srcNum; i++)
+			 {
+				arrayLinearList.add(0, new Integer(arrayLinearList.readNumber(scanner)));
+			 }
+		 // test size 
+		 if (arrayLinearList.isEmpty()) {
+			System.out.println("Баяртай :)");
+		 }
+		 else {
+			 System.out.println(arrayLinearList);
+			 System.out.println("Chain -ийн хэмжээ: " + arrayLinearList.size());
+			 flag = true;
+		 }
+		 
+		 if(flag) {
+//			 arrayLinearList.clearConsole();
+			 while(true) {
+				 try {
+					
+					 if (isShow) {
+						 
+						 System.out.println("\n-----------------------------");
+						 System.out.println(" Та сонголтоо хийнэ үү.");
+						 System.out.println("-----------------------------");
+						 
+			
+						 System.out.println("1. toArray-Массивруу хөрвүүлэх.");
+						 System.out.println("2. addRange-Шинэ элемент нэмэх.");
+						 System.out.println("3. union-Нэгдэл жагсаалтыг харах.");
+						 System.out.println("4. intersection-Огтлолцол жагсаалтыг харах.");
+						 System.out.println("7. Бүгд");
+						 System.out.println("0. Гарах");
+						 System.out.println("-----------------------------");
+
+					 } else {
+//						 System.out.println(arrayLinearList);
+						 System.out.println("0. Гарах");
+						 System.out.println("8. Буцах");
+					 }
+					 int selectedMethodNum = scanner.nextInt();
+					 
+					 switch (selectedMethodNum) {
+					 	case 0: {
+					 		System.out.println("Баяртай :)");
+					 		bye = true;
+					 		break;
+					 	}
+						case 1: {
+							System.out.println("Массивруу хөрвүүлэх.");
+							System.out.println(arrayLinearList.toArray(arrayLinearList));
+						     for(int i = 0; i < srcNum; i++) {
+						    	 System.out.println(arrayLinearList.toArray(arrayLinearList)[i]); 
+						     }
+						     isShow = false;
+						     break;
+						}
+						case 2: {
+							System.out.println("Шинэ элемент нэмэх.");
+							arrayLinearList.addRange(arrayLinearList); 
+							isShow = false;
+							break;
+						}
+						case 3: {
+							System.out.println("Нэгдэл жагсаалтыг харах.");
+					 		System.out.println("Нэгдэл жагсаалт: " + 
+							arrayLinearList.removeDuplicateElements(arrayLinearList.union(arrayLinearList))
+					 		);
+					 		System.out.println("\n\n");
+							isShow = false;
+							break;
+						}
+						case 4: {
+							System.out.println("Огтлолцол жагсаалтыг харах.");
+					 		System.out.println("Нэгдэл жагсаалт: " +
+							arrayLinearList.removeDuplicateElements(arrayLinearList.intersection(arrayLinearList))
+					 		);
+					 		System.out.println("\n\n");
+							isShow = false;
+							break;
+						}
+						case 8: {
+						     isShow = true;
+						     break;
+						}
+						default: {
+							if (selectedMethodNum < 0 && selectedMethodNum > 7) {
+								System.out.println("1-7 -ийн хооронд сонголт хийнэ үү.");
+							}
+							else {
+								System.out.println("-----------------------------");
+								System.out.println("\nПрограмаас гарлаа.");
+							}
+						}
+					 }
+					 if (bye) {
+				 		break; 
+					 }
+					 
+				 } 
+				 catch (Exception e) 
+				 {
+					 System.out.println(e); 
+					System.out.println("1-7 -ийн хооронд сонголт хийнэ үү.");
+					System.out.println("-----------------------------");
+					System.out.println("\nПрограмаас гарлаа.");
+				}
+			}
+		 }
+		 scanner.close();
+	}
 }
